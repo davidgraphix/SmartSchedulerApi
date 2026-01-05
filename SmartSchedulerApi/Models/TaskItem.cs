c4dae090044c41a2f48d00548a5fa4f5db9a28c3
David smart <112566573+davidgraphix@users.noreply.github.com>
2026-01-06 00:23:36 +0100

    Update Task Items

--- SmartSchedulerApi/Models/TaskItem.cs
+++ SmartSchedulerApi/Models/TaskItem.cs
@@ -2,5 +2,11 @@
 {
     public class TaskItem
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public bool IsCompleted { get; set; }
+        public int AssignedToId { get; set; }
+        public User AssignedTo { get; set; } = null!; 
     }
 }
