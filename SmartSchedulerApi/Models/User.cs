935ebab735f2fea97630aa3fe7ff107b993aae24
David smart <112566573+davidgraphix@users.noreply.github.com>
2026-01-06 00:16:57 +0100

    Update User Class

--- SmartSchedulerApi/Models/User.cs
+++ SmartSchedulerApi/Models/User.cs
@@ -2,5 +2,11 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public int RoleId { get; set; }
+        public string Role { get; set; } = null!;
     }
 }
