1a3998265fd87b58af332b7a7bf661334e4cbcb3
David smart <112566573+davidgraphix@users.noreply.github.com>
2026-01-06 09:45:45 +0100

    Created appointment class list

--- SmartSchedulerApi/Models/Appointment.cs
+++ SmartSchedulerApi/Models/Appointment.cs
@@ -2,5 +2,11 @@
 {
     public class Appointment
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
     }
 }
