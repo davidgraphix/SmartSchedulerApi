a7b2b8aef70c220e87b3e229cd903a81aced585f
David smart <112566573+davidgraphix@users.noreply.github.com>
2026-01-07 00:53:52 +0100

    Created Password Hashing

---
+++ SmartSchedulerApi/Helpers/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SmartSchedulerApi.Helpers
+{
+    public static class PasswordHasher
+    {
+        public static void CreatePasswordHash(string password, out byte[] hash, out byte[] salt)
+        {
+            using var hmac = new HMACSHA256();
+            salt = hmac.Key;
+            hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+        public static bool VerifyPassword(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            using var hmac = new HMACSHA256(storedSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(storedHash);
+        }
+
+    }
+}
